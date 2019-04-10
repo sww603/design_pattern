881de8b438d17579ef16c255024c73ed7c2b7639
sww <sww_603@163.com>
2019-04-10 18:04:40 +0800

    懒汉模式-双重检测

--- src/main/java/com/wenwen/design/pattern/Creational/singleton/LazySingleton.java
+++ src/main/java/com/wenwen/design/pattern/Creational/singleton/LazySingleton.java
@@ -2,15 +2,19 @@ package com.wenwen.design.pattern.Creational.singleton;
 
 /**
  * Created by sww_6 on 2019/4/9.
+ * 注意问题：
+ * 1、在静态方法（static）上添加关键字（synchronized同步锁），就是相当于在类上加锁，锁的范围大，损耗性能。
+ * 2、加锁、解锁过程消耗资源。
  */
 public class LazySingleton {
 
   private static LazySingleton lazySingleton = null;
-  private LazySingleton(){
+
+  private LazySingleton() {
 
   }
-  public static LazySingleton getInstance() {
-    if (lazySingleton == null){
+  public synchronized static LazySingleton getInstance() {
+    if (lazySingleton == null) {
       lazySingleton = new LazySingleton();
     }
     return lazySingleton;
